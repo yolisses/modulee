de909094ff6a4acff4dfff4094b0faacec5641c6
Yolisses <iamurise@gmail.com>
2025-11-30 10:55:53 -0300

    Fix HomeLogoButton on small screens

--- src/lib/project/ui/HomeLogoButton.svelte
+++ src/lib/project/ui/HomeLogoButton.svelte
@@ -1,8 +1,7 @@
-<a href="/projects" aria-label="Home" title="Go to projects page" class="navbar-item">
+<a href="/projects" aria-label="Home" title="Go to projects page" class="common-button">
 	<picture>
 		<source media="(max-width: 48rem)" srcset="/logo.svg" />
 		<source media="(min-width: 48.0625rem)" srcset="/logo-with-name-beta.svg" />
 		<img height="16" alt="Modulee logo" style="height: 1rem;" src="/logo.svg" />
 	</picture>
-	<div class="navbar-item-text md:hidden">Modulee</div>
 </a>
