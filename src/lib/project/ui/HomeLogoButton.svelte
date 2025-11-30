305a3d5fa8ac1a0489c9a752e3b405d013b4314f
Yolisses <iamurise@gmail.com>
2025-11-30 10:59:28 -0300

    Improve HomeLogoButton media query

--- src/lib/project/ui/HomeLogoButton.svelte
+++ src/lib/project/ui/HomeLogoButton.svelte
@@ -1,7 +1,7 @@
 <a href="/projects" aria-label="Home" title="Go to projects page" class="common-button">
 	<picture>
-		<source media="(max-width: 48rem)" srcset="/logo.svg" />
-		<source media="(min-width: 48.0625rem)" srcset="/logo-with-name-beta.svg" />
+		<source media="(min-width: 40rem)" srcset="/logo-with-name-beta.svg" />
+		<source srcset="/logo.svg" />
 		<img height="16" alt="Modulee logo" style="height: 1rem;" src="/logo.svg" />
 	</picture>
 </a>
